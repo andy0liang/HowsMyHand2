91f5714772b370a3f9754f2eacfb90ae2d0ebc53
andy0liang <andy0liang@gmail.com>
2022-02-14 06:15:04 -0800

    fixed bugs with sets and suits 1-5

--- src/Hand.java
+++ src/Hand.java
@@ -12,9 +12,9 @@ public class Hand {
 
 
     Spade = 4
-    Heart = 2
-    Club = 1
-    Diamond = 0
+    Heart = 3
+    Club = 2
+    Diamond = 1
      */
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
@@ -24,17 +24,21 @@ public class Hand {
     public static final String green = "\033[38;5;34m";
     private static final String[] names = new String[]{"invalid", "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
     private static final String[] shortnames = new String[]{"", "A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"};
-    private static final String[] suitnames = new String[]{"Diamonds", "Clubs", "Hearts", "Spades"};
-    private static final String[] shortsuits = new String[]{"D", "C", "H", "S"};
-    private static final String[] suiticons = new String[]{"♦", "♣", "♥", "♠"};
-    private final boolean[][] arr = new boolean[14][4];
+    private static final String[] suitnames = new String[]{"", "Diamonds", "Clubs", "Hearts", "Spades"};
+    private static final String[] shortsuits = new String[]{"", "D", "C", "H", "S"};
+    private static final String[] suiticons = new String[]{"", "♦", "♣", "♥", "♠"};
+    private final boolean[][] arr = new boolean[14][5];
     private int c1;
     private int c2;
     private final int[] community;
+    private long score = -1;
+    private String status = "unknown";
 
     public Hand(String card1, String card2) {
         this.c1 = cardStringToInt(card1);
         this.c2 = cardStringToInt(card2);
+        add(this.c1);
+        add(this.c2);
         community = new int[5];
     }
 
@@ -77,13 +81,13 @@ public class Hand {
 
     public String getSuitColor(int suitindex) {
         switch (suitindex) {
-            case 0:
+            case 4:
                 return ANSI_BLACK;
-            case 1:
+            case 3:
                 return red;
             case 2:
                 return green;
-            case 3:
+            case 1:
                 return blue;
             default:
                 return "";
@@ -114,8 +118,89 @@ public class Hand {
         return cardIntToShortString(this.c1) + " " + cardIntToShortString(this.c2);
     }
 
+    public String getCommunity(){
+        String result = "";
+        for(int x = 0; x < community.length; x++){
+            result += cardIntToShortString(community[x]) + " ";
+        }
+        return result;
+    }
+
+    public String getStatus(){
+        if(status.equals("unknown")){
+            getScore();
+        }
+        return status;
+    }
+
+    public long getScore(){
+        if(score != -1){
+            return score;
+        }
+        long temp;
+        temp = Sets.hasRoyalFlush(arr);
+        if(temp != -1){
+            status = "Royal Flush";
+            score = temp;
+            return temp;
+        }
+        temp = Sets.hasStraightFlush(arr);
+        if(temp != -1){
+            status = "Straight Flush";
+            score = temp;
+            return temp;
+        }
+        temp = Sets.has4ofAKind(arr);
+        if(temp != -1){
+            status = "4 of a Kind";
+            score = temp;
+            return temp;
+        }
+        temp = Sets.hasFullHouse(arr);
+        if(temp != -1){
+            status = "Full House";
+            score = temp;
+            return temp;
+        }
+        temp = Sets.hasFlush(arr);
+        if(temp != -1){
+            status = "Flush";
+            score = temp;
+            return temp;
+        }
+        temp = Sets.hasStraight(arr);
+        if(temp != -1){
+            status = "Straight";
+            score = temp;
+            return temp;
+        }
+        temp = Sets.has3ofAKind(arr);
+        if(temp != -1){
+            status = "3 of a Kind";
+            score = temp;
+            return temp;
+        }
+        temp = Sets.hasTwoPair(arr);
+        if(temp != -1){
+            status = "Two Pair";
+            score = temp;
+            return temp;
+        }
+        temp = Sets.hasPair(arr);
+        if(temp != -1){
+            status = "One Pair";
+            score = temp;
+            return temp;
+        }
+        temp = Sets.highCard(arr);
+        status = "High Card";
+        score = temp;
+        return temp;
+    }
+
+
     @Override
     public String toString() {
-        return getHand(true);
+        return getHand() + "\n" + getCommunity() + "\n" + getStatus();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,15 @@ public class Main {
         Hand hand = new Hand(input.nextLine(), input.nextLine());
         System.out.println(hand);
         System.out.println(hand.getHand());
+        System.out.println("Enter river: ");
+        hand.add(input.nextLine());
+        hand.add(input.nextLine());
+        hand.add(input.nextLine());
+        System.out.println("Enter turn: ");
+        hand.add(input.nextLine());
+        System.out.println("Enter river: ");
+        hand.add(input.nextLine());
+        System.out.println("score: "+hand.getScore());
+        System.out.println(hand);
     }
 }
